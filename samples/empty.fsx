dc58c376b53462e3679864f08f8e64868a0f5814
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2016-12-23 22:38:23 +0000

    update empty sample

--- samples/empty.fsx
+++ samples/empty.fsx
@@ -12,6 +12,14 @@ let rec private cache = new ConcurrentDictionary<Type, obj>()
 and private mkEmptyFunc<'T> () : unit -> 'T = cache.GetOrAdd(typeof<'T>, fun _ -> aux<'T> () :> obj) :?> _
 and private aux<'T> () : unit -> 'T =  
     let wrap (f : unit -> 'a) = unbox<unit -> 'T> f
+
+    let mkMemberInitializer (shape : IShapeWriteMember<'DeclaringType>) =
+        shape.Accept { new IWriteMemberVisitor<'DeclaringType, 'DeclaringType -> 'DeclaringType> with
+            member __.Visit (shape : ShapeWriteMember<'DeclaringType, 'Field>) =
+                let fe = mkEmptyFunc<'Field>()
+                fun inst -> shape.Inject inst (fe ())
+        }
+
     match TypeShape.Create<'T>() with
     | Shape.Bool -> wrap(fun () -> false)
     | Shape.Byte -> wrap(fun () -> 0uy)
@@ -34,80 +42,74 @@ and private aux<'T> () : unit -> 'T =
     | Shape.Unit -> wrap id
     | Shape.Enum s ->
         s.Accept { new IEnumVisitor<unit -> 'T> with
-            member __.Visit<'t, 'u when 't : enum<'u>>() =
+            member __.Visit<'t, 'u when 't : enum<'u>>() = // 'T = 't
                 let ue = mkEmptyFunc<'u>()
                 wrap(fun () -> LanguagePrimitives.EnumOfValue<'u,'t>(ue()))
         }
 
     | Shape.Nullable s ->
         s.Accept { new INullableVisitor<unit -> 'T> with
-            member __.Visit<'t when 't : struct and 't :> ValueType and 't : (new : unit -> 't)>() =
+            member __.Visit<'t when 't : struct and 't :> ValueType and 't : (new : unit -> 't)>() = // 'T = 't
                 wrap(fun () -> new Nullable<'t>())
         }
 
-    | Shape.Tuple2 s ->
-        s.Accept { new ITuple2Visitor<unit -> 'T> with
-            member __.Visit<'t1,'t2> () =
-                let et1,et2 = mkEmptyFunc<'t1>(), mkEmptyFunc<'t2>()
-                wrap(fun () -> et1 (), et2()) }
-
-    | Shape.Tuple3 s ->
-        s.Accept { new ITuple3Visitor<unit -> 'T> with
-            member __.Visit<'t1,'t2,'t3> () =
-                let et1,et2,et3 = mkEmptyFunc<'t1>(), mkEmptyFunc<'t2>(), mkEmptyFunc<'t3>()
-                wrap(fun () -> et1 (), et2(), et3()) }
-
-    | Shape.Tuple4 s ->
-        s.Accept { new ITuple4Visitor<unit -> 'T> with
-            member __.Visit<'t1,'t2,'t3,'t4> () =
-                let et1,et2,et3,et4 = mkEmptyFunc<'t1>(), mkEmptyFunc<'t2>(), mkEmptyFunc<'t3>(), mkEmptyFunc<'t4>()
-                wrap(fun () -> et1 (), et2(), et3(), et4()) }
-
     | Shape.FSharpOption s ->
         s.Accept { new IFSharpOptionVisitor<unit -> 'T> with
-            member __.Visit<'t>() =
+            member __.Visit<'t>() = // 'T = 't option
                 let et = mkEmptyFunc<'t>()
                 wrap(fun () -> Option<'t>.None) }
 
+    | Shape.KeyValuePair s ->
+        s.Accept { new IKeyValuePairVisitor<unit -> 'T> with
+            member __.Visit<'k,'v>() = // 'T = KeyValuePair<'k,'v>
+                let ke,ve = mkEmptyFunc<'k>(), mkEmptyFunc<'v>()
+                wrap(fun () -> new KeyValuePair<'k,'v>(ke(),ve())) }
+
     | Shape.FSharpList s ->
         s.Accept { new IFSharpListVisitor<unit -> 'T> with
-            member __.Visit<'t>() = wrap(fun () -> List.empty<'t>) }
+            member __.Visit<'t>() = wrap(fun () -> List.empty<'t>) } // 'T = 't list
 
     | Shape.Array s ->
         s.Accept { new IArrayVisitor<unit -> 'T> with
-            member __.Visit<'t>() = wrap(fun () -> Array.empty<'t>) }
+            member __.Visit<'t> rank = 
+                match rank with
+                | 1 -> wrap(fun () -> Array.empty<'t>) // 'T = 't []
+                | 2 -> wrap(fun () -> Array2D.zeroCreate<'t> 0 0) // 'T = 't [,]
+                | 3 -> wrap(fun () -> Array3D.zeroCreate<'t> 0 0 0) // 'T = 't [,,]
+                | 4 -> wrap(fun () -> Array4D.zeroCreate<'t> 0 0 0 0) // 'T = 't [,,,]
+                | _ -> failwithf "Unsupported type %O" typeof<'T> }
 
     | Shape.FSharpSet s ->
         s.Accept { new IFSharpSetVisitor<unit -> 'T> with
-            member __.Visit<'t when 't : comparison>() = wrap(fun () -> Set.empty<'t>) }
+            member __.Visit<'t when 't : comparison>() = wrap(fun () -> Set.empty<'t>) } // 'T = Set<'t>
 
     | Shape.FSharpMap s ->
         s.Accept { new IFSharpMapVisitor<unit -> 'T> with
-            member __.Visit<'K, 'V when 'K : comparison>() = wrap(fun () -> Map.empty<'K,'V>) }
+            member __.Visit<'k, 'v when 'k : comparison>() = wrap(fun () -> Map.empty<'k,'v>) } // 'T = Map<'k,'v>
 
-    | Shape.KeyValuePair s ->
-        s.Accept { new IKeyValuePairVisitor<unit -> 'T> with
-            member __.Visit<'K,'V>() = 
-                let ke,ve = mkEmptyFunc<'K>(), mkEmptyFunc<'V>()
-                wrap(fun () -> new KeyValuePair<'K,'V>(ke(),ve())) }
+    | Shape.Tuple s ->
+        s.Accept { new ITupleVisitor<unit -> 'T> with
+            member __.Visit (shape : ShapeTuple<'Tuple>) = // 'T = 'Tuple
+                let elemInitializers = shape.Elements |> Array.map mkMemberInitializer
+                wrap(fun () ->
+                    let mutable inst = shape.CreateUninitialized()
+                    for f in elemInitializers do inst <- f inst
+                    inst) }
 
-    | Shape.FSharpRecord s as ts ->
-        let mkEmptyField (p : PropertyInfo) =
-            TypeShape.Create(p.PropertyType).Accept 
-                { new ITypeShapeVisitor<unit -> obj> with
-                    member __.Visit<'T>() =
-                        let et = mkEmptyFunc<'T>()
-                        fun () -> et () :> obj }
 
-        ts.Accept { new ITypeShapeVisitor<unit -> 'T> with
-            member __.Visit<'t>() =
-                let fieldCtors = s.Properties |> Seq.map mkEmptyField |> Seq.toArray
-                wrap (fun () -> fieldCtors |> Array.map (fun f -> f ()) |> s.ConstructorInfo.Invoke :?> 't) }
+    | Shape.FSharpRecord s ->
+        s.Accept { new IFSharpRecordVisitor<unit -> 'T> with
+            member __.Visit (shape : ShapeFSharpRecord<'Record>) = // 'T = 'Record
+                let fieldInitializers = shape.Fields |> Array.map mkMemberInitializer
+                wrap(fun () ->
+                    let mutable inst = shape.CreateUninitialized()
+                    for f in fieldInitializers do inst <- f inst
+                    inst) }
 
-    | Shape.DefaultConstructor s ->
-        s.Accept { new IDefaultConstructorVisitor<unit -> 'T> with
-            member __.Visit<'t when 't : (new : unit -> 't)>() =
-                wrap(fun () -> new 't() |> unbox<'T>)
+    | Shape.CliMutable s ->
+        s.Accept { new ICliMutableVisitor<unit -> 'T> with
+            member __.Visit (shape : ShapeCliMutable<'Class>) = // 'T = 'Class
+                wrap(fun () -> shape.CreateUninitialized())
         }
 
     | _ -> failwithf "Type '%O' does not support empty values." typeof<'T>
