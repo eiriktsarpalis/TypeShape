091a19a02d129a6df5b7c7da33df1d1aa433c0a1
Eirik Tsarpalis <eirik.tsarpalis@jet.com>
2017-12-25 17:48:01 +0000

    add missing XML docs to TypeShape.CSharp

--- src/TypeShape.CSharp/ShapeSubtype.cs
+++ src/TypeShape.CSharp/ShapeSubtype.cs
@@ -47,27 +47,84 @@ namespace TypeShape
         }
     }
 
+    /// <summary>
+    ///     Subtype Visitor interface
+    /// </summary>
+    /// <typeparam name="TBase">Base type being visited.</typeparam>
+    /// <typeparam name="TResult">Result type of the visitor.</typeparam>
     public interface ISubtypeVisitor<TBase, TResult>
     {
+        /// <summary>
+        ///     Performs visitation by bringing the encapsulated subtype into scope.
+        /// </summary>
+        /// <typeparam name="TSubtype"></typeparam>
+        /// <returns></returns>
         TResult Visit<TSubtype>() where TSubtype : TBase;
     }
 
+    /// <summary>
+    ///     Subtype relationship visitor interface
+    /// </summary>
+    /// <typeparam name="TBase"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
     public interface ISubtypeWitnessVisitor<TBase, TResult>
     {
+        /// <summary>
+        ///     Visits the subtype witness by bringing the subtype type into scope.
+        /// </summary>
+        /// <typeparam name="TSubtype"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
         TResult Visit<TSubtype>(SubtypeWitness<TSubtype, TBase> container) where TSubtype : TBase;
     }
 
+    /// <summary>
+    ///     Interface that encapsulates a subtype relationship between an
+    ///     exposed TBase type and an encapsulated TSubtype type.
+    /// </summary>
+    /// <typeparam name="TBase"></typeparam>
     public interface IShapeSubtype<TBase>
     {
+        /// <summary>
+        ///     System.Type representation of the encapsulated subtype.
+        /// </summary>
         Type Subtype { get; }
+
+        /// <summary>
+        ///     Visits the subtype using a provided subtype visitor.
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="visitor"></param>
+        /// <returns></returns>
         TResult Accept<TResult>(ISubtypeVisitor<TBase, TResult> visitor);
+
+        /// <summary>
+        ///     Visits the subtype using a provided subtype witness visitor.
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="visitor"></param>
+        /// <returns></returns>
         TResult Accept<TResult>(ISubtypeWitnessVisitor<TBase, TResult> visitor);
     }
 
+    /// <summary>
+    ///     Concrete subtype shape implementation
+    /// </summary>
+    /// <typeparam name="TSubtype"></typeparam>
+    /// <typeparam name="TBase"></typeparam>
     public sealed class ShapeSubtype<TSubtype, TBase> : IShapeSubtype<TBase> where TSubtype : TBase
     {
+        /// <summary>
+        ///     System.Type representation of the encapsulated subtype
+        /// </summary>
         public Type Subtype => typeof(TSubtype);
 
+        /// <summary>
+        ///     Subtype visitor accept implementation
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="visitor"></param>
+        /// <returns></returns>
         public TResult Accept<TResult>(ISubtypeVisitor<TBase, TResult> visitor)
         {
             return visitor.Visit<TSubtype>();
