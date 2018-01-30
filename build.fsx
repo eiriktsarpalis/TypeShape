5fecd7e8bc3344595a47cb2eaf2ae35875998034
Eirik Tsarpalis <eirik.tsarpalis@jet.com>
2018-01-30 23:25:48 +0000

    disable sourceLink for NoEmit builds

--- build.fsx
+++ build.fsx
@@ -129,13 +129,13 @@ Target "Restore" (fun _ ->
 
 Target "Build" DoNothing
 
-let buildWithConfiguration config =
+let buildWithConfiguration sourceLink config =
     !! solutionFile
-    |> MSBuild null "Rebuild" [("Configuration", config) ; ("SourceLinkCreate", "true")]
+    |> MSBuild null "Rebuild" [("Configuration", config) ; ("SourceLinkCreate", sprintf "%b" sourceLink)]
     |> ignore  
 
-Target "Build.Emit" (fun _ -> buildWithConfiguration "Release")
-Target "Build.NoEmit" (fun _ -> buildWithConfiguration "Release-NoEmit")
+Target "Build.Emit" (fun _ -> buildWithConfiguration true "Release")
+Target "Build.NoEmit" (fun _ -> buildWithConfiguration false "Release-NoEmit")
 
 // --------------------------------------------------------------------------------------
 // Run the unit tests using test runner & kill test runner when complete
