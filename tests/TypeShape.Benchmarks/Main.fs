198473bb2e74e68823fe457f511af48fe04ce17b
Eirik Tsarpalis <eirik.tsarpalis@jet.com>
2018-03-25 03:44:33 +0100

    cleanup projects

--- tests/TypeShape.Benchmarks/Main.fs
+++ tests/TypeShape.Benchmarks/Main.fs
@@ -4,12 +4,7 @@ open BenchmarkDotNet.Running
 
 [<EntryPoint>]
 let main args =
-
-    printfn "%s" (typeof<FSharp.Quotations.Evaluator.QuotationEvaluator>.Assembly.Location)
-
     let assembly = System.Reflection.Assembly.GetExecutingAssembly()
     let switcher = new BenchmarkSwitcher(assembly)
     let summaries = switcher.Run(args)
-
-
     0
