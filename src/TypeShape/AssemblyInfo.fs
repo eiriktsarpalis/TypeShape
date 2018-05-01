012a05840cecf7c3fdfab10e5f740af1b547bf4b
Eirik Tsarpalis <eirik.tsarpalis@jet.com>
2018-05-01 21:48:13 +0100

    bump version

--- src/TypeShape/AssemblyInfo.fs
+++ src/TypeShape/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("TypeShape")>]
 [<assembly: AssemblyProductAttribute("TypeShape")>]
 [<assembly: AssemblyDescriptionAttribute("Practical Generic Programming in F#")>]
-[<assembly: AssemblyVersionAttribute("5.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("5.0.0")>]
+[<assembly: AssemblyVersionAttribute("5.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("5.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "TypeShape"
     let [<Literal>] AssemblyProduct = "TypeShape"
     let [<Literal>] AssemblyDescription = "Practical Generic Programming in F#"
-    let [<Literal>] AssemblyVersion = "5.0.0"
-    let [<Literal>] AssemblyFileVersion = "5.0.0"
+    let [<Literal>] AssemblyVersion = "5.1.0"
+    let [<Literal>] AssemblyFileVersion = "5.1.0"
