2c34b11595a68965b8b128708f3b6aab5b125038
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2019-10-08 00:01:08 +0100

    try targeting netcoreapp3.0 again

---
+++ foo.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+PAKET_EXE=.paket/paket.exe
+FAKE_FILE=build.fsx
+
+set -eu
+cd `dirname $0`
+
+which fake > /dev/null 2>&1 || (echo "fake-cli not found in PATH. Run 'dotnet tool install -g fake-cli'" && exit 1)
+
+OS=${OS:-"unknown"}
+
+run() {
+  if [ "$OS" != "Windows_NT" ]
+  then
+    mono "$@"
+  else
+    "$@"
+  fi
+}
+
+run $PAKET_EXE restore && \
+fake run $FAKE_FILE "$@"
