58ed1a83d28e427294922d06a8402e32ece9a6d3
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2019-10-10 15:47:28 +0100

    add baseline implementation

--- tests/TypeShape.Benchmarks/PrettyPrinter.fs
+++ tests/TypeShape.Benchmarks/PrettyPrinter.fs
@@ -13,6 +13,57 @@ type Union =
 
 type TestType = (struct(Record list list * string list [] * Union []))
 
+let baselinePrinter ((rss, sss, us) : TestType) =
+    let sb = new System.Text.StringBuilder()
+    let inline append (x:string) = sb.Append x |> ignore
+
+    let printRecord (r : Record) = sprintf "{ A = \"%s\" ; B = %d ; C = %b }" r.A r.B r.C |> append
+    let printUnion (u : Union) =
+        match u with
+        | A n -> sprintf "A %d" n |> append
+        | B -> append "B"
+        | C(x,y) -> sprintf "C(\"%s\", %d) x y" x y |> append
+
+    append "("
+    append "["
+    let mutable first = true
+    for rs in rss do
+        if first then first <- false else append "; "
+        append "["
+        let mutable first = true
+        for r in rs do
+            if first then first <- false else append "; "
+            printRecord r
+        append "]"
+    append "]"
+
+    append ", "
+    append "[|"
+    let mutable first = true
+    for ss in sss do
+        if first then first <- false else append "; "
+        append "["
+        let mutable first = true
+        for s in ss do
+            if first then first <- false else append "; "
+            append "\""
+            append s
+            append "\""
+        append "]"
+
+    append "|]"
+
+    append ", "
+    append "[|"
+    let mutable first = true
+    for u in us do
+        if first then first <- false else append "; "
+        printUnion u
+    append "|]"
+    append ")"
+
+    sb.ToString()
+
 let fsharpCorePrinter (value : TestType) =
     sprintf "%A" value
 
@@ -20,12 +71,14 @@ let typeShapePrinter = Printer.mkPrinter<TestType>()
 
 let testValue : TestType = 
     struct(
-        [ [{ A = "A" ; B = 42 ; C = false }]; []; [{A = "A'" ; B = 0 ; C = true}] ], 
+        [ [{ A = "value" ; B = 42 ; C = false }]; []; [{A = "A'" ; B = 0 ; C = true}] ],
         [| [] ; ["A";"B"] |], 
-        [|A 42; B; B ; C("C", 0)|])
+        [|A 42; B; B ; C("value", 0)|])
 
 type PrettyPrinterBenchmarks() =
-    [<Benchmark(Description = "FSharp.Core PrettyPrinter", Baseline = true)>]
+    [<Benchmark(Description = "Baseline PrettyPrinter", Baseline = true)>]
+    member __.Baseline() = baselinePrinter testValue |> ignore
+    [<Benchmark(Description = "FSharp.Core PrettyPrinter")>]
     member __.FSharpCore() = fsharpCorePrinter testValue |> ignore
     [<Benchmark(Description = "TypeShape PrettyPrinter")>]
     member __.TypeShape() = typeShapePrinter testValue |> ignore
