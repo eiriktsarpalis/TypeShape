9c3e090b15e6d5b91605c314fd49063b3ebad792
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2019-12-01 16:48:03 +0000

    nbe initial commit

---
+++ src/TypeShape/NBE.fs
@@ -0,0 +1,184 @@
+﻿module TypeShape.Core.NBE
+
+open System
+open System.Reflection
+open System.Collections.Generic
+open FSharp.Reflection
+open FSharp.Quotations
+open FSharp.Quotations.Patterns
+open FSharp.Quotations.DerivedPatterns
+open FSharp.Quotations.ExprShape
+
+// Expression tree denotation
+type Sem =
+    | VAR of Var * value:Sem option
+    | LIT of obj * Type
+    | LAM of Var * (Sem -> Sem)
+    | LET of Var * Sem * Sem
+    | TUPLE of Type * Sem list
+    | RECORD of Type * Sem list
+    | UNION of UnionCaseInfo * Sem list
+    //| OPERATOR of MethodInfo * Sem list
+    | SYN of shape:obj * Sem list
+
+let rec reify (rvars : HashSet<Var>) (s : Sem) : Expr =
+    match s with
+    | VAR (v, _) -> 
+        let _ = rvars.Add v
+        Expr.Var v
+    | LIT (o, t) -> Expr.Value(o, t)
+    | LAM (v, l) -> Expr.Lambda(v, l (VAR (v, None)) |> reify rvars)
+    | LET (v, b, k) ->
+        let eb = reify rvars b
+        let ek = reify rvars k
+        if rvars.Contains v then Expr.Let(v, eb, ek)
+        else ek
+
+    | TUPLE (_,es) -> Expr.NewTuple(es |> List.map (reify rvars))
+    | RECORD (t, fs) -> Expr.NewRecord(t, fs |> List.map (reify rvars))
+    | UNION (uci, fs) -> Expr.NewUnionCase(uci, fs |> List.map (reify rvars))
+    | SYN(shape, sparams) -> RebuildShapeCombination(shape, sparams |> List.map (reify rvars))
+
+let rec isValue (s : Sem) =
+    match s with
+    | LIT _ | LAM _ -> true
+    | VAR (_, v) -> v |> Option.forall isValue
+    | SYN _ | LET _ -> false
+    | TUPLE (_,fs) | RECORD (_,fs) | UNION(_,fs) -> fs |> List.forall isValue
+
+//| VAR of Var * value:Sem option
+//| LIT of obj * Type
+//| LAM of Var * (Sem -> Sem)
+//| LET of Var * Sem * Sem
+//| TUPLE of Type * Sem list
+//| RECORD of Type * Sem list
+//| UNION of UnionCaseInfo * Sem list
+//| SYN of shape:obj * Sem list
+
+//let isClosed (s : Sem) =
+//    let rec aux (env : Set<Var>) (s : Sem) =
+//        match s with
+//        | VAR v -> env.Contains v
+//        | LIT _ -> true
+//        | LAM(v, f) -> aux (env.Add v) (f (VAR v))
+//        | TUPLE fs
+//        | RECORD(_,fs)
+//        | UNION(_,fs) 
+//        | SYN(_,fs) -> fs |> List.forall (aux env)
+
+//    aux Set.empty s
+
+//let isLiteral s = isValue s && isClosed s
+
+type Environment = Map<Var, Sem>
+
+let rec meaning (env : Environment) (expr : Expr) : Sem =
+    let mkLit (x : 'T) = LIT(x, typeof<'T>)
+
+    let rec (|EV|) (s : Sem) =
+        match s with
+        | VAR(_, Some s) -> (|EV|) s
+        | _ -> s
+
+    let fallback (env : Environment) e =
+        match e with
+        | ShapeVar v -> 
+            match env.TryFind v with
+            | Some (LIT _ | VAR _ as s) -> s
+            | sopt -> VAR(v, sopt)
+        | ShapeLambda(v, body) -> LAM(v, fun s -> meaning (env.Add(v, s)) body)
+        | ShapeCombination(shape, args) ->
+            let sargs = args |> List.map (meaning env)
+            SYN(shape, sargs)
+        
+    match expr with
+    | Value(o, t) -> LIT(o, t)
+    | Application(f, g) ->
+        match meaning env f with
+        | EV (LAM (v, l)) ->
+            match meaning env g with
+            // (λ x. M) N ~> M[N/x]
+            | LIT _ | VAR _ | LAM _ as s -> l s
+            | s -> 
+                // (λ x. M) N ~> let x = N in M
+                LET(v, s, l (VAR(v, Some s)))
+        | _ -> fallback env expr
+
+    | Let(v, binding, body) when not v.IsMutable ->
+        let (EV s) = meaning env binding
+        if isValue s then
+            let sk = meaning (env.Add(v, s)) body
+            LET(v, s, sk)
+        else fallback env expr
+
+    | IfThenElse(cond, ifExpr, elseExpr) ->
+        match meaning env cond with
+        | EV (LIT(:? bool as ccond, _)) ->
+            let branch = if ccond then ifExpr else elseExpr
+            meaning env branch
+
+        | _ -> fallback env expr
+
+    | Sequential(left, right) ->
+        match meaning env left with
+        | s when isValue s -> meaning env right
+        | _ -> fallback env expr
+
+    // https://github.com/dotnet/fsharp/issues/7914
+    | NewTuple ts when not expr.Type.IsValueType -> 
+        TUPLE(expr.Type, ts |> List.map (meaning env))
+
+    | TupleGet(tuple, i) ->
+        match meaning env tuple with
+        | EV (TUPLE (_, ts)) -> ts.[i]
+        | _ -> fallback env expr
+
+    | NewRecord(t, fs) -> RECORD(t, fs |> List.map (meaning env))
+    | PropertyGet(Some e, prop, []) ->
+        match meaning env e with
+        | EV (RECORD(t,fs)) -> 
+            match FSharpType.GetRecordFields(t, true) |> Array.tryFindIndex(fun p -> prop = p) with
+            | Some i -> fs.[i]
+            | None -> fallback env expr
+        | EV (UNION(uci, fs)) ->
+            match uci.GetFields() |> Array.tryFindIndex(fun p -> prop = p) with
+            | Some i -> fs.[i]
+            | None -> fallback env expr
+        | _ -> fallback env expr
+
+    | NewUnionCase(uci, fs) -> UNION(uci, fs |> List.map (meaning env))
+    | UnionCaseTest(e, uci) ->
+        match meaning env e with
+        | EV (UNION(uci', _)) -> mkLit(uci = uci')
+        | _ -> fallback env expr
+
+    | TypeTest(e, t) ->
+        match meaning env e with
+        | EV (LIT(null,_)) -> mkLit(false)
+        | EV (LIT(o,_)) -> mkLit(t.IsAssignableFrom(o.GetType()))
+        | EV (TUPLE(tt,_)) -> mkLit(t = tt)
+        | EV (RECORD(rt,_)) -> mkLit(t = rt)
+        | EV (UNION(uci,_)) -> mkLit(uci.DeclaringType = t)
+        | _ -> fallback env expr
+
+    | SpecificCall <@ (|>) @> (None, _, [value; func])
+    | SpecificCall <@ (<|) @> (None, _, [func; value]) -> meaning env (Expr.Application(func, value))
+
+    | SpecificCall <@ ignore @> (None, _, [value]) -> meaning env (Expr.Sequential(value, Expr.Value(())))
+
+    | Call(None, mi, args) when mi.DeclaringType.FullName = "Microsoft.FSharp.Core.Operators" ->
+        let constArgs = 
+            args 
+            |> Seq.map (meaning env) 
+            |> Seq.choose (function EV (LIT(o,t)) when t.IsValueType -> Some o | _ -> None)
+            |> Seq.toArray
+
+        if constArgs.Length = args.Length then
+            let o = mi.Invoke(null, constArgs)
+            LIT(o, expr.Type)
+        else
+            fallback env expr
+
+    | _ -> fallback env expr
+
+let nbe (e : Expr<'a>) : Expr<'a> = reify (new HashSet<Var>()) (meaning Map.empty e) |> Expr.Cast
---
+++ tests/TypeShape.Tests/NBE.fs
@@ -0,0 +1,206 @@
+﻿module TypeShape.Tests.NBE
+
+open System
+open FSharp.Quotations
+open Swensen.Unquote
+open Xunit
+open TypeShape.Core.NBE
+
+#nowarn "0020"
+
+let shouldEqual (expected : Expr<'T>) (actual : Expr<'T>) = Assert.Equal(decompile expected, decompile actual)
+
+[<Fact>]
+let ``Constant expression`` () =
+    nbe <@ fun () -> 1 + 1 @> |> shouldEqual <@ fun () -> 2 @>
+
+[<Fact>]
+let ``Simple pipe elimination`` () =
+    let f x = x + 1
+    nbe <@ fun x -> x |> f @> |> shouldEqual <@ fun x -> f x @>
+    nbe <@ fun x -> f <| x @> |> shouldEqual <@ fun x -> f x @>
+
+[<Fact>]
+let ``Simple ignore elimination`` () =
+    nbe <@ ignore (1 + 1) @> |> shouldEqual <@ () @>
+    nbe <@ ignore (sprintf "hello") @> |> shouldEqual <@ sprintf "hello" ; () @>
+
+[<Fact>]
+let ``Let value elimination`` () =
+    nbe <@ fun () -> let x = 1 in x @> |> shouldEqual <@ fun () -> 1 @>
+
+[<Fact>]
+let ``Unused Let value elimination`` () =
+    nbe <@ fun () -> let f x = x + 2 in let y = f 2 in 1 @> |> shouldEqual <@ fun () -> 1 @>
+
+[<Fact>]
+let ``No mutable Let elimination`` () =
+    nbe <@ fun () -> let f x = x + 2 in let mutable y = f 2 in 1 @> 
+    |> shouldEqual <@ fun () -> let mutable y = 4 in 1 @> 
+
+[<Fact>]
+let ``No Let mutable elimination`` () =
+    nbe <@ fun () -> let f x = x + 2 in let mutable y = f 2 in 1 @> 
+    |> shouldEqual <@ fun () -> let mutable y = 4 in 1 @> 
+
+[<Fact>]
+let ``No side-effectful let elimination`` () =
+    nbe <@ fun x -> let f = printfn "hi" in x + 2 @> 
+    |> shouldEqual <@ fun x -> let f = printfn "hi" in x + 2 @> 
+
+[<Fact>]
+let ``Delayed side-effectful let elimination`` () =
+    nbe <@ fun x -> let f () = printfn "hi" in x + 2 @> 
+    |> shouldEqual <@ fun x -> x + 2 @> 
+
+[<Fact>]
+let ``Should not eliminate function if scope is escaped`` () =
+    nbe <@ fun x -> let f x = x + 1 in f, f x, f 1 @> |> shouldEqual <@ fun x -> let f x = x + 1 in f, x + 1, 2 @>
+
+[<Fact>]
+let ``Simple branch elimination`` () =
+    nbe <@
+        let f x =
+            if x > 0 then printfn "Positive"
+            elif x = 0 then printfn "Zero"
+            else printfn "Negative"
+
+        f -1; f 0; f 1
+    @>
+
+    |> shouldEqual <@ printfn "Negative" ; printfn "Zero" ; printfn "Positive" @>
+
+[<Fact>]
+let ``Simple Tuple elimination`` () =
+    nbe <@ fun x -> let z,_ = (x, "baz") in z + 1 @> |> shouldEqual <@ fun x -> x + 1 @>
+
+[<Fact>]
+let ``Combined Tuple, function and binding elimination`` () = 
+    nbe <@ fun x -> let y,f = 1, fun x y -> x + y in let g x = f x in (f x y, g x y) @> 
+    |> shouldEqual <@ fun x -> (x + 1, x + 1) @>
+
+[<Fact>]
+let ``Simple record elimination`` () =
+    nbe <@
+        fun z ->
+            let x = {| x = z + 1 |}
+            x.x
+    @> 
+    
+    |> shouldEqual <@ fun z -> z + 1 @>
+
+[<Fact>]
+let ``Simple record elimination 2`` () =
+    nbe <@
+        let x = {| x = 1 |}
+        let f (x : {| x : int |}) = x.x
+        (f x, f x)
+    @> 
+    
+    |> shouldEqual <@ (1,1) @>
+
+[<Fact>]
+let ``Should not eliminate record if escaping scope`` () =
+    nbe <@
+        let x = {| x = 1 |}
+        (x,x.x)
+    @> 
+    
+    |> shouldEqual <@ let x = {| x = 1 |} in (x,1) @>
+
+[<Fact>]
+let ``Simple pattern match elimination`` () =
+    nbe <@ 
+        fun x -> 
+            match Some x with 
+            | Some x -> x + 1 
+            | None -> -1 
+    @> 
+
+    |> shouldEqual <@ fun x -> x + 1 @>
+
+[<Fact>]
+let ``Parameterized pattern match`` () =
+    nbe <@ match Some 1 with Some x -> x + 1 | None -> -1 @> |> shouldEqual <@ 2 @>
+
+[<Fact>]
+let ``Simple beta reduction`` () =
+    nbe <@ fun z -> (fun x -> x + 1) z @>
+
+    |> shouldEqual <@ fun z -> z + 1 @>
+
+[<Fact>]
+let ``Beta reduction should respect evaluation semantics`` () =
+    nbe <@ fun x -> (fun x -> x,x) (printfn "hello" ; x + 1) @>
+
+    |> shouldEqual <@ fun x -> let x = printfn "hello" ; x + 1 in x,x @>
+
+[<Fact>]
+let ``Simple eta reduction`` () =
+    nbe <@ fun z -> let f x = x + z in fun x -> f x @>
+
+    |> shouldEqual <@ fun z x -> x + z @>
+
+[<Fact>]
+let ``Recursive function support`` () =
+    nbe <@ let rec f x = if x = 0 then (2 + (-1)) else x * f(x - 1) in f @>
+    |> shouldEqual <@ let rec f x = if x = 0 then 1 else x * f(x - 1) in f @>
+
+
+[<Fact>]
+let ``Imperative code support`` () =
+    nbe <@
+        fun x ->
+            let mutable isPrime = true
+            let mutable i = 1 + 1
+            while isPrime && i < x / (1 + 1) do
+                if x % i = 0 then isPrime <- false
+                else i <- i + (2 + (-1))
+
+            if isPrime then failwith "omg"
+    @> 
+    
+    |> shouldEqual <@ fun x ->
+        let mutable isPrime = true
+        let mutable i = 2
+        while isPrime && i < x / 2 do
+            if x % i = 0 then isPrime <- false
+            else i <- i + 1
+
+        if isPrime then failwith "omg"
+    @> 
+
+type Stream<'T> = ('T -> unit) -> unit
+
+[<Fact>]
+let ``Stream inlining`` () =
+    nbe <@
+        let ofList (xs : int []) =
+            fun k -> for x in xs do k x
+
+        let map : (int -> int) -> Stream<int> -> Stream<int> =
+            fun f ts k -> ts (fun t -> k (f t))
+
+        let filter : (int -> bool) -> Stream<int> -> Stream<int> =
+            fun f ts k -> ts (fun t -> if (f t) then k t)
+
+        let iter : (int -> unit) -> Stream<int> -> unit =
+            fun f ts -> ts f
+
+        fun xs ->
+            ofList xs
+            |> filter (fun i -> i > 0)
+            |> filter (fun i -> i % 2 = 0)
+            |> map (fun i -> i * i)
+            |> map (fun i -> i + i)
+            |> iter (fun i -> Console.WriteLine i)
+    @> 
+    
+    |> shouldEqual <@ fun (xs : int []) ->
+        for x in xs do
+            if x > 0 then
+                if x % 2 = 0 then
+                    let t = x * x
+                    let i = t + t
+                    Console.WriteLine i
+    @>
