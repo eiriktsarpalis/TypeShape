c2ee2d1b1da589bbb3a14e16791921b5d9ce0599
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-11-10 21:38:25 +0000

    remove .NET framework targets

--- tests/TypeShape.CSharp.Tests/CSharpRecords.cs
+++ tests/TypeShape.CSharp.Tests/CSharpRecords.cs
@@ -2,9 +2,6 @@
 
 namespace TypeShape.CSharp.Tests
 {
-    public class PlaceHolder { }
-
-#if NET5_0
     public record SimpleCSharpRecord(int x, string y, bool z);
 
     public record DerivedCSharpRecord(int x, string y, bool z, byte w) : SimpleCSharpRecord(x, y, z);
@@ -15,5 +12,4 @@ namespace TypeShape.CSharp.Tests
         public string Y { get; set; }
         public bool Z { get; }
     }
-#endif
 }
