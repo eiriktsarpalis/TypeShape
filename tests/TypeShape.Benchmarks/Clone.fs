f4e69ad1683c7ab69c35731cb2eac13f96fa7930
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2021-09-15 20:25:10 +0100

    remove unquote case from clone benchmarks

--- tests/TypeShape.Benchmarks/Clone.fs
+++ tests/TypeShape.Benchmarks/Clone.fs
@@ -1,6 +1,5 @@
 ﻿module TypeShape.Benchmarks.Clone
 
-open System
 open BenchmarkDotNet.Attributes
 
 open TypeShape.Clone
@@ -11,39 +10,34 @@ type Record =
 
 type TestType = (struct(Record list list * string list [] * int option []))
 
-let baselineCloner : TestType -> TestType =
-    fun (struct(v1,v2,v3)) ->
-
-        let cloneRecord (r : Record) =
-            { A = r.A ; B = r.B ; C = r.C }
-
-        let v1' = v1 |> List.map (List.map cloneRecord)
-        let v2' = v2 |> Array.map (List.map id)
-        let v3' = v3 |> Array.map id
-        struct(v1',v2',v3')
-
-let typeShapeCloner : TestType -> TestType =
-    clone<TestType>
-
-let unquoteStagedCloner : TestType -> TestType =
-    mkStagedCloner<TestType>()
-
-let compiledStagedCloner : TestType -> TestType =
-    mkCompiledCloner<TestType>()
-
-
-let testValue : TestType =
-    let rs = [ for i in 1 .. 100 -> { A = sprintf "lorem ipsum %d" i ; B = i ; C = i % 2 = 0 } ]
-    let ss = [for i in 1 .. 20 -> string i]
-    struct([rs; []], [|ss|], [|for i in 1 .. 20 -> Some i|])
-
 [<MemoryDiagnoser>]
 type CloneBenchmarks() =
+
+    let baselineCloner : TestType -> TestType =
+        fun (struct(v1,v2,v3)) ->
+
+            let cloneRecord (r : Record) =
+                { A = r.A ; B = r.B ; C = r.C }
+
+            let v1' = v1 |> List.map (List.map cloneRecord)
+            let v2' = v2 |> Array.map (List.map id)
+            let v3' = v3 |> Array.map id
+            struct(v1',v2',v3')
+
+    let typeShapeCloner : TestType -> TestType =
+        clone<TestType>
+
+    let compiledStagedCloner : TestType -> TestType =
+        mkCompiledCloner<TestType>()
+
+    let testValue : TestType =
+        let rs = [ for i in 1 .. 100 -> { A = sprintf "lorem ipsum %d" i ; B = i ; C = i % 2 = 0 } ]
+        let ss = [for i in 1 .. 20 -> string i]
+        struct([rs; []], [|ss|], [|for i in 1 .. 20 -> Some i|])
+
     [<Benchmark(Description = "Baseline Cloner", Baseline = true)>]
     member _.Baseline() = baselineCloner testValue |> ignore
     [<Benchmark(Description = "TypeShape Cloner")>]
-    member _.Reflection() = typeShapeCloner testValue |> ignore
-    [<Benchmark(Description = "TypeShape Unquote Staged Cloner")>]
-    member _.Unquote() = unquoteStagedCloner testValue |> ignore
-    [<Benchmark(Description = "TypeShape Compiled Staged Cloner")>]
-    member _.TypeShape() = compiledStagedCloner testValue |> ignore
+    member _.TypeShape() = typeShapeCloner testValue |> ignore
+    [<Benchmark(Description = "TypeShape Staged Cloner with compilation")>]
+    member _.TypeShape_Staged() = compiledStagedCloner testValue |> ignore
